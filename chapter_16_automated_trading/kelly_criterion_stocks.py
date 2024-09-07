d3d7cdfebc75fbcc789eaca8dee6fe15081c92da
valeriu <valeriu.langa@yahoo.com>
2024-09-07 18:14:31 +0100

    * fixed params for crypto

--- chapter_16_automated_trading/kelly_criterion_stocks.py
+++ chapter_16_automated_trading/kelly_criterion_stocks.py
@@ -22,10 +22,8 @@ data = pd.read_parquet("E:\\coding\\analytical_tools\\data\\SOL-USD_2024-07-21_t
 data['start'] = pd.to_datetime(data['start'])
 data.set_index('start', inplace=True)
 data = data.drop(labels=['low', 'high', 'open', 'volume'], axis=1)
-print(data.dtypes)
 data = data.sort_index(ascending=True)
 
-print(data.tail())
 # SPX data
 # data.set_index('date', inplace=True)
 # data = data.drop(labels=['open', 'high', 'low', 'adjusted_close', 'volume'], axis=1)
@@ -34,32 +32,18 @@ data = data.rename({'close' : symbol}, axis=1)
 
 data['returns'] = np.log(data / data.shift(1))
 data = data.dropna()
-"""
-             SPX   returns
-date
-2023-09-06  9910  0.000808  <- initial spx level
-2023-09-07  9882 -0.002829
-2023-09-08  9984  0.010269
-2023-09-11  9876 -0.010876
-2023-09-12  9800 -0.007725
-...          ...       ...
-2024-08-28  7410  0.009492
-2024-08-29  7700  0.038390
-2024-08-30  7695 -0.000650
-2024-09-02  7620 -0.009794
-2024-09-03  7600 -0.002628  <- note that the spx lost money in this run
-"""
 
-mu = data.returns.mean() * 252              # annualized return as there are 252 trading days
+mu = data.returns.mean() * 365              # (for SOL) 365 days since crypto trades non stop
+                                            # (for SPX) annualized return as there are 252 trading days
 print(mu)
-# -0.2645885096469468
-sigma = data.returns.std() * 252 ** 0.5     # annualized volatility 
+# 0.4785953143436269
+sigma = data.returns.std() * 365 ** 0.5     # annualized volatility 
 print(sigma)
-# 0.2638898840606999
+# 0.45626009321926025
 r = 0.0                                     # risk free rate; simplified
 f = (mu - r) / sigma ** 2                   # optimal kelly fraction
 print(f)
-# -3.7994916586484413                       # negative kelly since spx was on a downturn
+# 2.2990238286500344
 
 equs = []
 def kelly_strategy(f):
@@ -82,15 +66,15 @@ def kelly_strategy(f):
 kelly_strategy(f * 0.5)     # half-kelly
 kelly_strategy(f * 0.66)    # 2/3 kelly
 kelly_strategy(f)           # full kelly
-# print(data[equs].tail())
+print(data[equs].tail())
 """
-            equity_-1.90  equity_-2.51  equity_-3.80
-datelogit
-2024-08-28      1.443798      1.543761      1.640657
-2024-08-29      1.336453      1.392255      1.396694
-2024-08-30      1.338102      1.394522      1.400140
-2024-09-02      1.362878      1.428606      1.451990
-2024-09-03      1.369674      1.438008      1.466470
+                     equity_1.15  equity_1.52  equity_2.30
+start
+2024-07-20 00:00:00     8.333963    13.026132    23.216090
+2024-07-20 06:00:00     8.226354    12.804115    22.616555
+2024-07-20 12:00:00     8.574504    13.519406    24.530878
+2024-07-20 18:00:00     8.538317    13.444091    24.323822
+2024-07-21 00:00:00     8.511757    13.388888    24.172492  <- overfitting to say the least 
 """
 
 ax = data['returns'].cumsum().apply(np.exp).plot(legend=True, figsize=(10,6))
